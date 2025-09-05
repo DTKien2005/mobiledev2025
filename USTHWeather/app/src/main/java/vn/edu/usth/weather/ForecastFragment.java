ca66929e89c78490f46b93581237ba1f5471da83
unknown <kiendt2005.ict@gmail.com>
2025-09-05 13:50:14 +0700

    Complete the Pratical 4

--- USTHWeather/app/src/main/java/vn/edu/usth/weather/ForecastFragment.java
+++ USTHWeather/app/src/main/java/vn/edu/usth/weather/ForecastFragment.java
@@ -27,7 +27,7 @@ public class ForecastFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_forecast, container, false);
-        view.setBackgroundColor(0x20FF0000);
+        view.setBackgroundColor(0xFFFFFFFF);
 
         TextView dayTextView = view.findViewById(R.id.dayTextView);
         ImageView weatherIcon = view.findViewById(R.id.weatherIcon);
